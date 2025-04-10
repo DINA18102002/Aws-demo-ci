1f0c098291e2d91f4b29a71bb1254778244b5ee1
DINA18102002 <theenaxz@gmail.com>
2025-04-10 17:50:56 +0000

    Update message to Hello, Theena!

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, AWS CodePipeline!");
+        System.out.println("Hello, Theena!");
     }
 }
